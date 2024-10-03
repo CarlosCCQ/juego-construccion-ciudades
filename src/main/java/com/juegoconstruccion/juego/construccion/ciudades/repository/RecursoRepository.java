5c39fad0c7db1f4b876cf0c95c7c5816f034a168
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-03 18:27:47 -0500

    Actualizacion de RecursoRepository

--- src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/RecursoRepository.java
+++ src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/RecursoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RecursoRepository extends JpaRepository {
+public interface RecursoRepository extends JpaRepository<Recurso, Long> {
     List<Recurso> findByTipoRecursosAndCiudad(Tipo_recurso tipoRecursos, Ciudad ciudad);
     List<Recurso> findByTipoRecursos(Tipo_recurso tipoRecursos);
 }
