5c2569d77f61aa80dc762d037bfb15ba4ad0cd55
KevinPV11 <kpalominov@uni.pe>
2024-10-03 20:37:01 -0500

    Add CiudadRepository

---
+++ src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/CiudadRepository.java
@@ -0,0 +1,13 @@
+package com.juegoconstruccion.juego.construccion.ciudades.repository;
+
+import com.juegoconstruccion.juego.construccion.ciudades.model.Ciudad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CiudadRepository extends JpaRepository<Ciudad, Long> {
+    List<Ciudad> findByNombre(String nombreCiudad);
+}
