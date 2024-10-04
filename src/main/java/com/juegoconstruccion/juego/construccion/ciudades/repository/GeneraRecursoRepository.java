60ed75ea8d8ed8eb94cec769374f0a4165a26848
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-04 15:24:06 -0500

    Documentacion de los reposiotiros de GeneraRecursoRepository y RecursoRepository

--- src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/GeneraRecursoRepository.java
+++ src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/GeneraRecursoRepository.java
@@ -6,6 +6,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Interfaz que define los métodos para interactuar con la base de datos
+ * relacionada con los generadores de recursos.
+ *
+ * Extiende JpaRepository para proporcionar operaciones CRUD
+ * básicas y consultas personalizadas para la entidad Genera_recuso.
+ */
 public interface GeneraRecursoRepository extends JpaRepository<Genera_recuso, Long> {
+    /**
+     * Busca todos los generadores de recursos asociados a una ciudad específica.
+     *
+     * @param ciudad La ciudad para la cual se desean obtener los generadores de recursos.
+     * @return Una lista de generadores de recursos (Genera_recuso) asociados a la ciudad proporcionada.
+     */
     List<Genera_recuso> findByCiudad(Ciudad ciudad);
 }
--- src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/RecursoRepository.java
+++ src/main/java/com/juegoconstruccion/juego/construccion/ciudades/repository/RecursoRepository.java
@@ -7,7 +7,28 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+/**
+ * Interfaz que define los métodos para interactuar con la base de datos
+ * relacionada con los recursos.
+ *
+ * Extiende JpaRepository para proporcionar operaciones CRUD
+ * básicas y consultas personalizadas para la entidad Recurso.
+ */
 public interface RecursoRepository extends JpaRepository<Recurso, Long> {
+    /**
+     * Busca todos los recursos de un tipo específico que pertenecen a una ciudad específica.
+     *
+     * @param tipoRecursos El tipo de recurso que se desea buscar.
+     * @param ciudad La ciudad para la cual se desean obtener los recursos.
+     * @return Una lista de recursos (Recurso) que coinciden con el tipo y la ciudad proporcionados.
+     */
     List<Recurso> findByTipoRecursosAndCiudad(Tipo_recurso tipoRecursos, Ciudad ciudad);
+
+    /**
+     * Busca todos los recursos de un tipo específico sin filtrar por ciudad.
+     *
+     * @param tipoRecursos El tipo de recurso que se desea buscar.
+     * @return Una lista de recursos (Recurso) que coinciden con el tipo proporcionado.
+     */
     List<Recurso> findByTipoRecursos(Tipo_recurso tipoRecursos);
 }
