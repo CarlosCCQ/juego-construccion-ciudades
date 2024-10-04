d12c82d049bc4c48224fe6b3d19a8e4cad46f3b8
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-04 15:20:45 -0500

    Documentacion de las clases enum Tipo_generador_recurso y Tipo_recurso

--- src/main/java/com/juegoconstruccion/juego/construccion/ciudades/model/Tipo_generador_recurso.java
+++ src/main/java/com/juegoconstruccion/juego/construccion/ciudades/model/Tipo_generador_recurso.java
@@ -1,5 +1,26 @@
 package com.juegoconstruccion.juego.construccion.ciudades.model;
 
+/**
+ * Enumeración que define los tipos de generadores de recursos en el juego.
+ * Los generadores de recursos son entidades que producen diferentes tipos de recursos
+ * utilizados en la construcción y el desarrollo de ciudades.
+ */
 public enum Tipo_generador_recurso {
-    CANTERAS, MINAS, RIO
+    /**
+     * Representa un generador de recursos de tipo cantera,
+     * que produce recursos relacionados con la minería de piedra.
+     */
+    CANTERAS,
+
+    /**
+     * Representa un generador de recursos de tipo mina,
+     * que produce recursos relacionados con la minería de metales como el oro.
+     */
+    MINAS,
+
+    /**
+     * Representa un generador de recursos de tipo río,
+     * que produce recursos relacionados con el agua.
+     */
+    RIO
 }
--- src/main/java/com/juegoconstruccion/juego/construccion/ciudades/model/Tipo_recurso.java
+++ src/main/java/com/juegoconstruccion/juego/construccion/ciudades/model/Tipo_recurso.java
@@ -1,5 +1,26 @@
 package com.juegoconstruccion.juego.construccion.ciudades.model;
 
+/**
+ * Enumeración que define los tipos de recursos disponibles en el juego.
+ * Los recursos son elementos esenciales que se utilizan en la construcción
+ * y el desarrollo de ciudades.
+ */
 public enum Tipo_recurso {
-    PIEDRA, ORO, AGUA
+    /**
+     * Representa el recurso de tipo piedra,
+     * utilizado para diversas construcciones.
+     */
+    PIEDRA,
+
+    /**
+     * Representa el recurso de tipo oro,
+     * requerido para la posiblidad de construccion de un edificio.
+     */
+    ORO,
+
+    /**
+     * Representa el recurso de tipo agua,
+     * requerido para la posibilidad de construir un edificio.
+     */
+    AGUA
 }
