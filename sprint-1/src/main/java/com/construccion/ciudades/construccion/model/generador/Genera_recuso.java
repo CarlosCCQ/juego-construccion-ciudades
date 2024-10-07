8f3dc01fea37c8d67f54b92757f80e1ac8c546da
KevinPV11 <kpalominov@uni.pe>
2024-10-07 16:50:44 -0500

    Add CiudadReporsitoryTest.java

--- sprint-1/src/main/java/com/construccion/ciudades/construccion/model/generador/Genera_recuso.java
+++ sprint-1/src/main/java/com/construccion/ciudades/construccion/model/generador/Genera_recuso.java
@@ -8,7 +8,7 @@ import jakarta.persistence.*;
 @Table(name = "generador")
 public class Genera_recuso {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Enumerated(EnumType.STRING)
