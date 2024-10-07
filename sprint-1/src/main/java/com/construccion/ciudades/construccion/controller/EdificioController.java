0b41ad51fbd147879de74348d827e6b206f54476
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-07 10:57:02 -0500

    Creacion de los controles para las peticiones http

---
+++ sprint-1/src/main/java/com/construccion/ciudades/construccion/controller/EdificioController.java
@@ -0,0 +1,43 @@
+package com.construccion.ciudades.construccion.controller;
+
+import com.construccion.ciudades.construccion.dto.EdificioDto;
+import com.construccion.ciudades.construccion.service.edificioservice.IEdificioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/edificios")
+public class EdificioController {
+    @Autowired
+    private IEdificioService edificioService;
+
+    @GetMapping
+    public List<EdificioDto> obtenerTodosLosEdificios() {
+        return edificioService.obtenerTodosLosEdificios();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EdificioDto> obtenerEdificioPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(edificioService.obtenerEdificioPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<EdificioDto> crearEdificio(@RequestBody EdificioDto edificioDto) {
+        return new ResponseEntity<>(edificioService.crearEdificio(edificioDto), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EdificioDto> actualizarEdificio(@PathVariable Long id, @RequestBody EdificioDto edificioDto) {
+        return ResponseEntity.ok(edificioService.actualizarEdificio(id, edificioDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarEdificio(@PathVariable Long id) {
+        edificioService.eliminarEdificio(id);
+        return ResponseEntity.noContent().build();
+    }
+}
