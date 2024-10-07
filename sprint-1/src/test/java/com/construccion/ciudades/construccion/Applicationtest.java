bc9673269143c2d141f19ed99270fed167eb3e85
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-07 12:02:53 -0500

    Creacion de las historias de usuario

--- sprint-1/src/test/java/com/construccion/ciudades/construccion/Applicationtest.java
+++ sprint-1/src/test/java/com/construccion/ciudades/construccion/Applicationtest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
+public class Applicationtest {
+    @Test
+    void contextLoads() {
+    }
 
 }
