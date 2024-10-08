3807d56c042ad67f59eb2376443cde87ec3c721b
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-10-08 18:29:13 -0500

    Add method up level

--- Sprint-2/src/main/java/com/build/api/service/ciudadservice/CiudadService.java
+++ Sprint-2/src/main/java/com/build/api/service/ciudadservice/CiudadService.java
@@ -38,6 +38,8 @@ public class CiudadService implements ICiudadService{
     @Autowired
     private EdificioRepository edificioRepository;
 
+    private static final int OBJETIVO_EDIFICIOS = 5;
+
     @Override
     @Transactional
     public List<CiudadDto> obtenerTodasLasCiudades() {
@@ -209,5 +211,23 @@ public class CiudadService implements ICiudadService{
         Ciudad ciudad = ciudadRepository.findByNombre(nombreCiudad).stream().findFirst().orElseThrow(()-> new RuntimeException("Ciudad no encontrada"));
 
         return convertirACiudadDto(ciudad);
+    }
+
+    @Override
+    public boolean verificarYSubirNivelCiudad(Long ciudadId, int objetivoEdificios) {
+        Ciudad ciudad = ciudadRepository.findById(ciudadId)
+                .orElseThrow(() -> new RuntimeException("Ciudad no encontrada"));
+
+        if (ciudad.haAlcanzadoObjetivoDeEdificios(objetivoEdificios)) { // Usar el parámetro objetivoEdificios
+            ciudad.incrementarNivel();
+            otorgarRecompensa(ciudad);
+            ciudadRepository.save(ciudad);
+            return true;
+        }
+        return false;
+    }
+
+    private void otorgarRecompensa(Ciudad ciudad) {
+        ciudad.getRecursos().forEach(recurso -> recurso.setCantidad(recurso.getCantidad() + 10));
     }
 }
