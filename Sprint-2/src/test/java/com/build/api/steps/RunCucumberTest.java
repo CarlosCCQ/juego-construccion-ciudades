bea8a4df5417cada8204c63ebf62e67ecac67692
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-10-08 15:28:14 -0500

    Add cucumber configuration

---
+++ Sprint-2/src/test/java/com/build/api/steps/RunCucumberTest.java
@@ -0,0 +1,14 @@
+package com.build.api.steps;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/resources/features/construccion_edifico.feature",
+        glue = {"com.build.api.steps"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+public class RunCucumberTest {
+}
