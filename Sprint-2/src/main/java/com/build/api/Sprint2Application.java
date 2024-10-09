9ccf2bafb24b07a1e474725687644c0eda3e088d
HumbleG0d <95192731+HumbleG0d@users.noreply.github.com>
2024-10-08 20:54:54 -0500

    Git add anotation SpringBootApplication

--- Sprint-2/src/main/java/com/build/api/Sprint2Application.java
+++ Sprint-2/src/main/java/com/build/api/Sprint2Application.java
@@ -1,7 +1,9 @@
 package com.build.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Sprint2Application {
 
 	public static void main(String[] args) {
